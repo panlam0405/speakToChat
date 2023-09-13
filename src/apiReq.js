b227f3bbc4dabc56348f995d36b034a597c43a87
Timos <timos@design-vs.com>
2023-09-13 13:16:30 +0300

    reformat files

--- src/apiReq.js
+++ src/apiReq.js
@@ -1,44 +1,44 @@
-import { Configuration, OpenAIApi } from "openai";
-import { synth, voices } from "./main";
+import {Configuration, OpenAIApi} from "openai";
+import {synth, voices} from "./main";
 
 const configuration = new Configuration({
-	organization: "org-2zUXztWSzyCkiThKfsJDfe7f",
-	apiKey: import.meta.env.VITE_OPENAI_API_KEY,
+    organization: "org-2zUXztWSzyCkiThKfsJDfe7f",
+    apiKey: import.meta.env.VITE_OPENAI_API_KEY,
 });
 
 const openai = new OpenAIApi(configuration);
 
 export async function runCompletion(text) {
-	const gtpResponse = await openai.createChatCompletion({
-		model: "gpt-3.5-turbo",
-		messages: [{ role: "user", content: text }],
-	});
-	const { choices } = await gtpResponse.data;
+    const gtpResponse = await openai.createChatCompletion({
+        model: "gpt-3.5-turbo",
+        messages: [{role: "user", content: text}],
+    });
+    const {choices} = await gtpResponse.data;
 
-	const response = choices[0]?.message?.content;
+    const response = choices[0]?.message?.content;
 
-	console.log(response);
-	readMessage(response);
+    console.log(response);
+    readMessage(response);
 }
 
 function setVoices() {
-	if (voices.length === 0) {
-		alert("Sorry, it seems this browser does not support different voices.");
-		// voiceSelect.remove();
-	}
+    if (voices.length === 0) {
+        alert("Sorry, it seems this browser does not support different voices.");
+        // voiceSelect.remove();
+    }
 
-	// for (let i = 0; i < voices.length; i++) {
-	// 	const option = document.createElement("option");
-	// 	option.textContent = `${voices[i].name} (${voices[i].lang})`;
+    // for (let i = 0; i < voices.length; i++) {
+    // 	const option = document.createElement("option");
+    // 	option.textContent = `${voices[i].name} (${voices[i].lang})`;
 
-	// 	if (voices[i].default) {
-	// 		option.textContent += " — DEFAULT";
-	// 	}
+    // 	if (voices[i].default) {
+    // 		option.textContent += " — DEFAULT";
+    // 	}
 
-	// 	option.setAttribute("data-lang", voices[i].lang);
-	// 	option.setAttribute("data-name", voices[i].name);
-	// 	voiceSelect.appendChild(option);
-	// }
+    // 	option.setAttribute("data-lang", voices[i].lang);
+    // 	option.setAttribute("data-name", voices[i].name);
+    // 	voiceSelect.appendChild(option);
+    // }
 }
 
 // function getSelectedVoice() {
@@ -47,9 +47,9 @@ function setVoices() {
 // }
 
 function readMessage(message) {
-	const msg = new SpeechSynthesisUtterance();
-	msg.text = message;
-	msg.lang = document.getElementById("lang").value;
-	// msg.voice = getSelectedVoice();
-	synth.speak(msg);
+    const msg = new SpeechSynthesisUtterance();
+    msg.text = message;
+    msg.lang = document.getElementById("lang").value;
+    // msg.voice = getSelectedVoice();
+    synth.speak(msg);
 }
--- src/main.js
+++ src/main.js
@@ -1,57 +1,59 @@
-import { runCompletion } from "./apiReq";
+import {runCompletion} from "./apiReq";
 import "./styles.scss";
+
 export let voices = [];
 export let synth;
 
 if (!"speechSynthesis" in window) {
-	alert("Sorry, your browser doesn't support text to speech!");
+    alert("Sorry, your browser doesn't support text to speech!");
 }
 synth = window.speechSynthesis;
 voices = synth.getVoices();
 
 const recognitionSvc =
-	window.SpeechRecognition || window.webkitSpeechRecognition;
+    window.SpeechRecognition || window.webkitSpeechRecognition;
 let recognition;
 try {
-	recognition = new recognitionSvc();
+    recognition = new recognitionSvc();
 } catch (error) {
-	document.getElementById(
-		"transcription"
-	).innerHTML = `<p>This browser does not support speech recognition API. Please, use another one (Google Chrome recommended)</p>`;
+    document.getElementById(
+        "transcription"
+    ).innerHTML = `<p>This browser does not support speech recognition API. Please, use another one (Google Chrome recommended)</p>`;
 }
 document.querySelector("#start").addEventListener("click", () => {
-	const startBtn = document.querySelector("#start");
-	recognition.lang = document.querySelector("#lang").value;
-	recognition.continuous = true;
+    const startBtn = document.querySelector("#start");
+    recognition.lang = document.querySelector("#lang").value;
+    recognition.continuous = true;
 
-	recognition.onresult = (event) => {
-		const accumulatedResult = [];
-		for (const result of event.results)
-			accumulatedResult.push(`${result[0].transcript}`);
+    recognition.onresult = (event) => {
+        const accumulatedResult = [];
+        for (const result of event.results)
+            accumulatedResult.push(`${result[0].transcript}`);
 
-		document.querySelector("#transcription").innerHTML = accumulatedResult;
-	};
+        document.querySelector("#transcription").innerHTML = accumulatedResult;
+    };
 
-	if (startBtn.classList.contains("listening")) recognition.stop();
-	else recognition.start();
+    if (startBtn.classList.contains("listening")) recognition.stop();
+    else recognition.start();
 
-	startBtn.classList.toggle("listening");
+    startBtn.classList.toggle("listening");
 });
 
 document
-	.querySelector("#transcription")
-	.addEventListener("click", async (e) => {
-		const text = e.target.innerText;
-		const gtpResponse = await runCompletion(text);
-		console.log(gtpResponse);
-		navigator.clipboard.writeText(e.target.innerText).then(() => {
-			document.getElementById("tooltip").classList.add("active");
-			setTimeout(() => {
-				document.getElementById("tooltip").classList.remove("active");
-			}, 3100);
-		});
-	});
+    .querySelector("#transcription")
+    .addEventListener("click", async (e) => {
+        const text = e.target.innerText;
+        console.log("Start")
+        const gtpResponse = await runCompletion(text);
+        console.log(gtpResponse);
+        navigator.clipboard.writeText(e.target.innerText).then(() => {
+            document.getElementById("tooltip").classList.add("active");
+            setTimeout(() => {
+                document.getElementById("tooltip").classList.remove("active");
+            }, 3100);
+        });
+    });
 
 setTimeout(() => {
-	document.getElementById("tooltip").classList.remove("init");
+    document.getElementById("tooltip").classList.remove("init");
 }, 3100);
